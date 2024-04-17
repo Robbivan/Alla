ef56131c71ab91ca1d2f7e7acbb8cd57aaa8d402
Robbivan <96996213+robbivan@users.noreply.github.com>
2024-04-18 00:23:51 +0300

    Refactoring in top_sort

--- graph_algo.py
+++ graph_algo.py
@@ -4,56 +4,19 @@ class Algo:
 
 
 class AlgoGraph(Algo):
-    # def top_sort(self, graph_in_algo, start):
-    #
-    #     visited = set()
-    #     stack = [start]
-    #     stack_calls = []
-    #     order = []
-    #     topological_queue = []
-    #
-    #     while stack:
-    #         vertex = stack.pop()
-    #         if vertex not in visited:  # если vertex уже в visited, то это цикл
-    #             # print(vertex.get_name())
-    #
-    #             visited.add(vertex)
-    #
-    #             neighbors = graph_in_algo.get(vertex, [])
-    #             stack_calls.append(vertex)
-    #             len_stack = len(stack)
-    #             # if neighbors.empty()
-    #
-    #             for neighbor, _ in reversed(neighbors):
-    #                 if neighbor not in visited:
-    #                     stack.append(neighbor)
-    #                 if len_stack == len(stack):
-    #                     stack_calls.append(stack.pop())
-    #
-    #             # stack.append((vertex, BLACK))
-    #             topological_queue.append(vertex)
-    #     return topological_queue
     def top_sort(self, graph_in_algo, start):
 
         visited = set()
-        stack = [start]
+        stack_vertex = [start]
         stack_calls = []
         order = []
 
-        # vertex = stack.pop()
-        # first_elements = [item[0] for item in graph_in_algo[vertex]]
-        # print(first_elements)
-
-        while stack:
-            vertex = stack.pop()
+        while stack_vertex:
+            vertex = stack_vertex.pop()
             if vertex not in visited:  # если vertex уже в visited, то это цикл
-
                 visited.add(vertex)
 
-                neighbors = [item[0] for item in graph_in_algo[vertex]]
-                # neighbors = graph_in_algo.get(vertex, [])
-                stack.extend(neighbors)
-
+                stack_vertex.extend([item[0] for item in graph_in_algo[vertex]])
 
                 while stack_calls and vertex not in [item[0] for item in graph_in_algo[stack_calls[-1]]]:
                     order.append(stack_calls.pop())
