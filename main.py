2b8e94f442200fb02f7ea1b83326160dcc26ff9e
Ivan <kbasille@mail.ru>
2024-04-26 17:55:51 +0300

    Added gui to diff file

--- main.py
+++ main.py
@@ -1,41 +1,10 @@
-import os
-import webbrowser
-
 from program_ini import ProgramIni
 from load_from_json import *
 from graph_algo import AlgoGraph
-from pyvis.network import Network
+from gui import do_gui
 
 
-def do_gui(program, graph, sort_list_graph):
-    net = Network(notebook=True)  # отображение в Блокноте
 
-    net.add_nodes([item.id for item in sort_list_graph],
-                  label=[item.tag for item in sort_list_graph])
-
-    keys = graph.keys()
-
-    for micro in keys:
-        for link in graph[micro]:
-            net.add_edge(micro.id, link[0].id)
-    print(graph)
-
-
-    # net.add_nodes([1, 2, 3],
-    #               value=[10, 100, 400],
-    #               title=['I am node 1', 'node 2 here', 'and im node 3'],
-    #               x=[21.4, 54.2, 11.2],
-    #               y=[100.2, 23.54, 32.1],
-    #               label=['NODE 1', 'NODE 2', 'NODE 3'],
-    #               color=['#00ff1e', '#162347', '#dd4b39'])
-
-    net.show("html/result.html")
-
-    current_directory = os.path.dirname(__file__)
-
-    html_directory = os.path.join(current_directory, 'html')
-    file_path = os.path.join(html_directory, 'result.html')
-    webbrowser.open(file_path, new=2)  # open in new tab
 
 
 def do_hidden_work(program):
